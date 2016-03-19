f3e88a4976c86cde61a2f9b638d7bb598a5db0b0
Herwig Hochleitner <herwig@bendlas.net>
2016-03-19 15:03:45 +0100

    update data.xml

--- project.clj
+++ project.clj
@@ -5,16 +5,15 @@
     - A connector to expose files over webdav"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src/server"]
+  :source-paths ["src/server" "src/client"]
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [net.bendlas/data.xml "1.0.0"]
-                 [webnf/base "0.1.19-SNAPSHOT"
-                  :exclusions [org.clojure/data.xml]]
+                 [org.clojure/data.xml "0.1.0-beta1"]
+                 [webnf/base "0.1.19-SNAPSHOT"]
                  [webnf/handler "0.1.19-SNAPSHOT"]
                  [webnf/datomic "0.1.19-SNAPSHOT"]
                  [webnf/enlive.clj "0.1.19-SNAPSHOT"]
-                 [webnf.deps/universe "0.1.19-SNAPSHOT"
-                  :exclusions [org.clojure/data.xml]]
+                 [webnf.deps/universe "0.1.19-SNAPSHOT"]
+                 [webnf/cljs "0.1.19-SNAPSHOT"]
                  [ring/ring-jetty-adapter "1.4.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler davstore.app/davstore
