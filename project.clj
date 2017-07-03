0e39c6421843073a2858f79531bf7673284bbe62
Herwig Hochleitner <herwig@bendlas.net>
2017-07-03 19:29:21 +0200

    update versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject webnf/davstore "0.2.0-alpha2"
+(defproject webnf/davstore "0.2.0-alpha3-SNAPSHOT"
   :plugins [[lein-modules "0.3.11"]]
   :description "A file storage component with three parts:
     - A blob store, storing in a git-like content-addressing scheme
@@ -7,7 +7,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/server" "src/client" "src/common"]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/data.xml "0.2.0-alpha2"]
                  [webnf/base "_" :upgrade false]
                  [webnf/handler "_" :upgrade false]
@@ -15,8 +15,8 @@
                  [webnf/enlive  "_" :upgrade false]
                  [webnf/filestore  "_" :upgrade false]]
   :profiles
-  {:dev {:plugins [[lein-ring "0.11.0"]]
+  {:dev {:plugins [[lein-ring "0.12.0"]]
          :source-paths ["src/devel"]
-         :dependencies [[ring/ring-jetty-adapter "1.6.0-beta7"]]
+         :dependencies [[ring/ring-jetty-adapter "1.6.1"]]
          :ring {:handler webnf.davstore.test/davstore
                 :nrepl {:start? true :port 4012}}}})
